3afeb6da168858223af5d8d390d300ed6340b82f
Sh4Lf <sergeinemchinov@mail.ru>
2024-10-12 20:17:52 +0600

    Add files via upload

---
+++ Module_7_4.py
@@ -0,0 +1,24 @@
+tasks_total = 82
+time_avg = 45.2
+
+print(f"Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!")
+
+score_1 = 40
+score_2 = 42
+
+print(f"Команды решили {score_1} и {score_2} задач.")
+
+team2_time = 2153.31451
+team1_time = 1552.512
+
+print("Мастера кода решили задачи за {} с !".format(team2_time))
+print("Волшебники данных решили задачи за {:.2f} с !".format(team1_time))
+
+team1_num = 5
+team2_num = 6
+
+print("В команде Мастера кода участников: %s !" % team1_num)
+print("В команде Волшебники данных: %s !" % (team2_num))
+
+challenge_result = "победа команды Волшебники данных!"
+print(f"Результат битвы: {challenge_result}")
